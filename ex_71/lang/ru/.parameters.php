05207041cdca5de1cf3a7b57d2ed38a929e456a1
Elena-Orlova-Bitrix <134793619+Elena-Orlova-Bitrix@users.noreply.github.com>
2023-05-27 19:39:43 +0300

    Create .parameters.php

---
+++ ex_71/lang/ru/.parameters.php
@@ -0,0 +1,6 @@
+<?
+$MESS["SIMPLECOMP_EXAM2_CATALOG_IBLOCK_ID"] = "ID инфоблока с каталогом товаров";
+$MESS["SIMPLECOMP_EXAM2_CLASSIFIER_IBLOCK_ID"] = "ID инфоблока классификатора";
+$MESS["SIMPLECOMP_EXAM2_CATALOG_IBLOCK_DETAIL_URL"] = "Шаблон ссылки на детальный просмотр товара";
+$MESS["SIMPLECOMP_EXAM2_CLASSIFIER_PROPERTY_CODE"] = "Код свойства товара привязки к классификатору";
+?>
